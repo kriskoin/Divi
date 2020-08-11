8b2abd148fcdbdd1c54fd7dfcfe6c99ee989f98e
kriskoin <kriskoin>
2020-08-10 19:01:52 -0600

    upd: bool DisconnectBlock removed from main.cpp

---
+++ divi/src/blockHeaders.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2017-2020 The DIVI Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef _BLOCKHEADERS_H
+#define _BLOCKHEADERS_H
+
+#include <uint256.h>
+#include <BlockUndo.h>
+
+class CBlock;
+class CValidationState;
+class CBlockIndex;
+class CCoinsViewCache;
+
+/** Functions for validating blocks and updating the block tree */
+
+/** Undo the effects of this block (with given index) on the UTXO set represented by coins.
+ *  In case pfClean is provided, operation will try to be tolerant about errors, and *pfClean
+ *  will be true if no problems were found. Otherwise, the return value will be false in case
+ *  of problems. Note that in any case, coins may be modified. */
+bool DisconnectBlock(CBlock& block, CValidationState& state, CBlockIndex* pindex, CCoinsViewCache& coins, bool* pfClean = NULL);
+
+#endif
