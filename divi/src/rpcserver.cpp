b712fa13c7c58e1549c050c56ff80ab9854f09dc
galp <the.talion@gmail.com>
2020-09-15 10:11:13 -0600

    Remove autocombine dust from wallet

--- divi/src/rpcserver.cpp
+++ divi/src/rpcserver.cpp
@@ -369,7 +369,6 @@ static const CRPCCommand vRPCCommands[] =
 
         /* Wallet */
         {"wallet", "addmultisigaddress", &addmultisigaddress, true, false, true},
-        {"wallet", "autocombinerewards", &autocombinerewards, false, false, true},
         {"wallet", "backupwallet", &backupwallet, true, false, true},
         {"wallet", "dumpprivkey", &dumpprivkey, true, false, true},
         {"wallet", "dumphdinfo", &dumphdinfo, true, false, true},
