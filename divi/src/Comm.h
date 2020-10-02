ccc78b498d0dd558ec8fcda91690de61ebb4af79
kriskoin <cris7775@gmail.com>
2020-10-02 12:19:37 -0600

    Create files for the class Ccomm

---
+++ divi/src/Comm.h
@@ -0,0 +1,9 @@
+
+// Copyright (c) 2017-2020 The DIVI Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef _COMM_H
+#define _COMM_H
+
+#endif
