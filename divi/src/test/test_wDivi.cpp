487ebe671b20830f461e87acf6b8af448ff6f015
kriskoin <cris7775@gmail.com>
2020-11-20 12:08:13 -0600

    new: First version of the wDivi unit tests

---
+++ divi/src/test/test_wDivi.cpp
@@ -0,0 +1,124 @@
+// Copyright (c) 2017-2020 The Divi Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#define BOOST_TEST_MODULE Divi Test Suite
+
+#include "main.h"
+#include "random.h"
+#include "txdb.h"
+#include "ui_interface.h"
+#include "util.h"
+#ifdef ENABLE_WALLET
+#include "db.h"
+#include "wallet.h"
+#endif
+
+#include <boost/filesystem.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+
+void MineGenesis(CBlock genesis)
+{
+    printf("Searching for genesis block...\n");
+    // This will figure out a valid hash and Nonce if you're
+    // creating a different genesis block:
+    uint256 hashTarget = ~uint256(0) >> 20;
+    uint256 thash;
+    while(true)
+    {
+        thash = genesis.GetHash();
+        if (thash <= hashTarget)
+            break;
+        if ((genesis.nNonce & 0xFFF) == 0)
+        {
+            printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+        }
+        ++genesis.nNonce;
+        if (genesis.nNonce == 0)
+        {
+            printf("NONCE WRAPPED, incrementing time\n");
+            ++genesis.nTime;
+        }
+    }
+    printf("block.nTime = %u \n", genesis.nTime);
+    printf("block.nNonce = %u \n", genesis.nNonce);
+    printf("block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+    printf("block.merkle = %s\n", genesis.hashMerkleRoot.ToString().c_str());
+    std::fflush(stdout);
+}
+
+CClientUIInterface uiInterface;
+CWallet* pwalletMain;
+
+extern bool fPrintToConsole;
+extern void noui_connect();
+extern NotificationInterfaceRegistry registry;//TODO: rid this
+
+struct TestingWdivi {
+    CCoinsViewDB *pcoinsdbview;
+    boost::filesystem::path pathTemp;
+    boost::thread_group threadGroup;
+
+    TestingSetup() {
+        SetupEnvironment();
+        fPrintToDebugLog = false; // don't want to write to debug.log file
+        fCheckBlockIndex = true;
+        SelectParams(CBaseChainParams::UNITTEST);
+        noui_connect();
+#ifdef ENABLE_WALLET
+        CDB::bitdb.MakeMock();
+#endif
+        pathTemp = GetTempPath() / strprintf("test_divi_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
+        boost::filesystem::create_directories(pathTemp);
+        SetParameter("-datadir", pathTemp.string());
+        pblocktree = new CBlockTreeDB(1 << 20, true);
+        pcoinsdbview = new CCoinsViewDB(1 << 23, true);
+        pcoinsTip = new CCoinsViewCache(pcoinsdbview);
+        InitBlockIndex();
+#ifdef ENABLE_WALLET
+        bool fFirstRun;
+        pwalletMain = new CWallet("wallet.dat");
+        pwalletMain->LoadWallet(fFirstRun);
+        RegisterValidationInterface(pwalletMain);
+#endif
+        nScriptCheckThreads = 3;
+        for (int i=0; i < nScriptCheckThreads-1; i++)
+            threadGroup.create_thread(&ThreadScriptCheck);
+        RegisterNodeSignals(GetNodeSignals());
+    }
+    ~TestingWdivi()
+    {
+        threadGroup.interrupt_all();
+        threadGroup.join_all();
+        UnregisterNodeSignals(GetNodeSignals());
+#ifdef ENABLE_WALLET
+        delete pwalletMain;
+        pwalletMain = NULL;
+#endif
+        delete pcoinsTip;
+        delete pcoinsdbview;
+        delete pblocktree;
+#ifdef ENABLE_WALLET
+        CDB::bitdb.Flush(true);
+#endif
+        boost::filesystem::remove_all(pathTemp);
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(TestingWDivi);
+
+void Shutdown(void* parg)
+{
+  exit(0);
+}
+
+void StartShutdown()
+{
+  exit(0);
+}
+
+bool ShutdownRequested()
+{
+  return false;
+}
